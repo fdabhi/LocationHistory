0dfc9d7ccf1ef0cb8f7f2737d0505164da68587c
Abhishek Padlekar <suprrasaprra@gmail.com>
2017-02-02 15:18:51 +0530

    Add files via upload

---
+++ location.py
@@ -0,0 +1,33 @@
+import json
+
+fh = open("LocationHistory.json")
+data = fh.read()
+print 'Retrieved',len(data),'characters'
+js = json.loads(data)
+
+location = js["locations"]
+tp=[]
+for loc in location:
+    if "activitys" in loc:
+        acti = loc["activitys"]
+        for act in acti:
+            if "activities" in act:
+                types = act["activities"]
+                for typ in types:
+                    #if typ["confidence"] ==100:
+                    tp.append(typ["type"])
+
+#print tp
+counts = {}
+for i in tp:
+    counts[i]=counts.get(i,0)+1
+print counts
+word = ""
+count = 0
+for k,v in counts.items():
+    if word =="" or v > count:
+        word = k
+        count = v
+
+
+print word ,count
